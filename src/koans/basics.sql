d07a70b3b4868bc497352ccfe48d279005f4a245
pxggleswxggles <45026750+pxggleswxggles@users.noreply.github.com>
2024-02-03 17:23:22 -0500

    Update basics.sql

--- src/koans/basics.sql
+++ src/koans/basics.sql
@@ -2,22 +2,23 @@
 SELECT 1
 
 -- Meditate on lower-case queries
-_____ 1
+select 1
 
 -- Meditate on selecting all columns from a table
 select * from book
 
 -- Meditate on selecting one column (title) from a table
-select _____ from book
+select title from book
 
 -- Meditate on selecting some columns (title, available) from a table
-select _____, _____ from book
+select title, available from book
 
 -- Meditate on adding a new book
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
+values('Some New Book', 'Fantasy', 2015, 1)
 
 -- Meditate on adding two new books
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
-,('_____', '_____', _____, 1)
+values('Some New Book', 'Fantasy', 2015, 1)
+,('Another New Book', 'Fantasy', 1999, 1)
+
